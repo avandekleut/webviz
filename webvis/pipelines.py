31f93ad2b14a3859f4f13e8d012f994dce8bb1b1
Alexander Van de Kleut <54694031+avandekleut@users.noreply.github.com>
2023-03-11 17:34:19 -0500

    working well

--- webvis/pipelines.py
+++ webvis/pipelines.py
@@ -22,8 +22,10 @@ class PyVisPipeline:
     def close_spider(self, spider):
         print('close_spider')
         print('count', self.count)
-        self.compute_communities()
-        self.compute_sizes()
+        self.update_and_save_network()
+
+    def update_and_save_network(self):
+        self.update_nodes_and_edges()
         self.save_network()
 
     def process_item(self, item: WebvisItem, spider):
@@ -31,38 +33,90 @@ class PyVisPipeline:
 
         self.nx.add_edge(item['source'], item['dest'])
 
+        if self.count % self.save_frequency == 0:
+            self.update_and_save_network()
+
         return item
 
-    def compute_communities(self, iters=5):
+    def update_nodes_and_edges(self, iters=7, desired_elements_per_community=None):
+        if iters and desired_elements_per_community:
+            raise Exception(
+                f'expected one of: iters, desired_elements_per_community. got: {iters}, {desired_elements_per_community}')
+
+        if desired_elements_per_community is not None:
+            communities = self.get_communities_by_number_of_desired_elements_per_community(
+                5)
+        else:
+            communities = self.get_communities(iters)
+        self.update_node_groups(communities)
+        # self.update_edge_weights_by_community_membership(communities)
+
+        self.update_node_sizes()
+
+    def get_communities_by_number_of_desired_elements_per_community(self, desired=5):
+        """
+        if after i iterations there are i + 1 communities and we want each community
+        to hold d desired items, then the expected number of iterations is c = len(V) // d
+        where len(V) is the number of nodes in the graph.
+
+        This would yield c communities, each containing d elements, so that d * c = d * len(V) // d ~= len(V)
+        """
+        iters = len(self.nx.nodes) // desired
+        return self.get_communities(iters)
+
+    def get_communities(self, iters=5):
         assert iters > 0, f'expected: {iters} > 0'
 
         community_generator = girvan_newman(self.nx)
 
         # run the algorithm for iters - 1 iterations
+        # girvan_newman: each iteration produces exactly one
+        # more community.
         for iter in range(iters - 1):
             next(community_generator)
 
         communities = []
-        for i, community in enumerate(next(community_generator)):
+        for community in next(community_generator):
             community = list(community)
             communities.append(community)
 
+        return communities
+
+    def update_node_groups(self, communities):
+        for i, community in enumerate(communities):
             for node in community:
                 self.nx.nodes[node]['group'] = i
 
-        print(f'found {len(communities)} communities in {iters} iterations')
-        print(communities)
+    def update_edge_weights_by_community_membership(self, communities):
+        """
+        update all edge weights to be 1 between members of a community and 0 
+        between non-members
+        """
+        pass
+        # for node in self.nx.nodes:
+        # for
+        # self.nx.nodes[node]['weight'] = 100
 
-    def compute_sizes(self, base_size=2):
+    def update_node_sizes(self):
         for node in self.nx.nodes:
-            neighbors = nx.all_neighbors(self.nx, node)
-            num_neighbors = len(list(neighbors))
-            node_size = base_size + num_neighbors
-            self.nx.nodes[node]['size'] = node_size
+            size = self.get_node_size(node)
+            self.nx.nodes[node]['size'] = size
+
+    def get_node_size(self, node):
+        size = self.get_num_neighbours(node)
+        return self.normalize_size(size)
+
+    def get_num_neighbours(self, node):
+        neighbors = nx.all_neighbors(self.nx, node)
+        num_neighbors = len(list(neighbors))
+        return num_neighbors
+
+    def normalize_size(self, size, base_size=2):
+        return base_size + size
 
     def save_network(self, filename='out.html'):
         net = Network(
-            # directed=True,
+            # directed=True, # interesting but distracting
             select_menu=True
         )
         net.from_nx(self.nx)
